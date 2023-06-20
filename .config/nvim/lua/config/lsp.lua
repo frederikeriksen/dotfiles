016edcc8c12be0485a424160a42bc573297a9939
Frederik Julius Eriksen <frederikjulius94@gmail.com>
2023-06-20 17:42:22 +0200

    Update lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,5 +3,9 @@ require('mason-lspconfig').setup({
     ensure_installed = {
         'lua_ls',
         'gopls',
+        'pyright',
+        'tsserver',
+        'cssls',
+        'html'
     }
 })
